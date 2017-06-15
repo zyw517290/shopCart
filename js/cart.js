6613c7f8cbc2b497988a4ac8e8460626ca15785a
matrixation <25352820@qq.com>
2017-06-15 10:42:36 +0800

    add cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,21 @@
 /**
  * Created by matrix on 2017/6/14.
  */
+var vm = new Vue({
+    el: "#app",
+    data: {
+        productList:[]
+    },
+    mounted:function () {
+        this.cartView();
+    },
+    methods: {
+        cartView: function () {
+            var _this = this;
+            this.$http.get('data/cartData.json').then(function (res) {
+                console.log(res.data);
+                // _this.productList = res.result.list;
+            })
+        }
+    }
+});
